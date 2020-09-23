073bf309d7cb526ad234b8e47cf11eb7894fa6c6
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-09-23 11:55:52 -0400

    final solution using object literal for duplicates lookup

--- CountDuplicates.js
+++ CountDuplicates.js
@@ -1,8 +1,22 @@
 class CountDuplicates {
-
-    countDups(input){
-        return null;
+  countDups(input) {
+    let count = 0;
+    let duplicates = {};
+    for (let integer of input) {
+      if (!duplicates[integer]) {
+        duplicates[integer] = 1;
+      } else {
+        duplicates[integer] += 1;
+      }
     }
+
+    for (let key in duplicates) {
+      if (duplicates[key] >= 2) {
+        count += 1;
+      }
+    }
+    return count;
+  }
 }
 
 module.exports = CountDuplicates;
